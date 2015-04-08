f591e3afd94d17366e051e9cc90ae23e528347d7
proger10 <vladimirsitnov@gmail.com>
2015-04-08 16:03:08 +0600

    Update main.c

--- 4. horse/main.c
+++ 4. horse/main.c
@@ -6,14 +6,29 @@
 #include "console.h"
 
 #define MAP_SIZE 8
+#define COLOR_HORSE 1
+#define COLOR_CHECKED 2
+#define COLOR_BORDER 3
+#define COLOR_NORMAL 4
 
+<<<<<<< HEAD
+static void init_colors() {
+	//con_initPair(COLOR_HORSE, CON_COLOR_BLUE, CON_COLOR_BLACK);
+	con_initPair(COLOR_HORSE, CON_COLOR_WHITE, CON_COLOR_BLUE);
+	con_initPair(COLOR_CHECKED, CON_COLOR_BLUE, CON_COLOR_BLUE);
+	con_initPair(COLOR_BORDER, CON_COLOR_WHITE, CON_COLOR_BLACK);
+	con_initPair(COLOR_NORMAL, CON_COLOR_WHITE, CON_COLOR_BLACK);
+}
+
+=======
+>>>>>>> origin/master
 typedef struct _point{
 	int x;
 	int y;
 } point;
 
 char check_point(int x, int y){
-	if ((x > MAP_SIZE - 1) || (y > MAP_SIZE-1) || (x < 0) || (y < 0))
+	if ((x > MAP_SIZE - 1) || (y > MAP_SIZE - 1) || (x < 0) || (y < 0))
 		return 0;
 	return 1;
 }
@@ -26,8 +41,8 @@ char solve(int x, int y, char *map, int *empty, point **result){
 	(*empty)--;
 
 	if (*empty != 0){
-		for (int i = 0; i<sizeof(moves)/sizeof(point); i++){
-			if ((x + moves[i].x > MAP_SIZE - 1) || (y + moves[i].y > MAP_SIZE-1) || (x + moves[i].x < 0) || (y + moves[i].y < 0))
+		for (int i = 0; i<sizeof(moves) / sizeof(point); i++){
+			if ((x + moves[i].x > MAP_SIZE - 1) || (y + moves[i].y > MAP_SIZE - 1) || (x + moves[i].x < 0) || (y + moves[i].y < 0))
 				continue;
 			if (map[MAP_SIZE * (x + moves[i].x) + y + moves[i].y])
 				continue;
@@ -56,6 +71,59 @@ char solve(int x, int y, char *map, int *empty, point **result){
 }
 
 void print_horse(point *result){
+<<<<<<< HEAD
+#define CORNER_POS_X  3
+#define CORNER_POS_Y  3
+#define PIXEL_RATIO_X 2
+#define PIXEL_RATIO_Y 1
+	con_init();
+	con_hideCursor();
+	con_clearScr();
+	init_colors();
+	con_setColor(COLOR_BORDER);
+	for (int i = 0; i < (8 + 2)*PIXEL_RATIO_X; i++){
+		for (int j = 0; j < PIXEL_RATIO_Y; j++){
+			con_gotoXY(CORNER_POS_X + i, CORNER_POS_Y+j);
+			con_outTxt("#");
+			con_gotoXY(CORNER_POS_X + i, CORNER_POS_Y + j+9*PIXEL_RATIO_Y);
+			con_outTxt("#");
+		}
+
+	}
+	for (int i = 0; i < (8 + 2)*PIXEL_RATIO_Y; i++){
+		for (int j = 0; j < PIXEL_RATIO_X; j++){
+			con_gotoXY(CORNER_POS_X + j, CORNER_POS_Y + i);
+			con_outTxt("#");
+			con_gotoXY(CORNER_POS_X + j + 9 * PIXEL_RATIO_X, CORNER_POS_Y + i);
+			con_outTxt("#");
+		}
+
+	}
+
+	for (int step = MAP_SIZE*MAP_SIZE - 1; step >= 0; step--){
+
+		con_setColor(COLOR_HORSE);
+		for (int i = 0; i < PIXEL_RATIO_X; i++){
+			for (int j = 0; j < PIXEL_RATIO_Y; j++){
+				con_gotoXY(CORNER_POS_X + PIXEL_RATIO_X + result[step].x * PIXEL_RATIO_X+i, CORNER_POS_Y + PIXEL_RATIO_Y + result[step].y * PIXEL_RATIO_Y+j);
+				con_outTxt("!");
+			}
+		}
+
+		Sleep(500);
+
+		con_setColor(COLOR_CHECKED);
+		for (int i = 0; i < PIXEL_RATIO_X; i++){
+			for (int j = 0; j < PIXEL_RATIO_Y; j++){
+				con_gotoXY(CORNER_POS_X + PIXEL_RATIO_X + result[step].x * PIXEL_RATIO_X+i, CORNER_POS_Y + PIXEL_RATIO_Y + result[step].y * PIXEL_RATIO_Y+j);
+				con_outTxt(" ");
+			}
+		}
+	}
+	con_gotoXY(0, CORNER_POS_Y + PIXEL_RATIO_Y *10);
+	con_setColor(COLOR_NORMAL);
+	con_deinit();
+=======
 	con_init();
 	con_hideCursor();
 	con_clearScr();
@@ -84,6 +152,7 @@ void print_horse(point *result){
 		con_outTxt(" ");
 	}
 	getc(stdin);
+>>>>>>> origin/master
 }
 
 void horse(int x, int y){
@@ -97,8 +166,6 @@ void horse(int x, int y){
 	solve(x - 1, y - 1, map, &empty, &result);
 	free(map);
 	if (result == NULL){
-		//con_gotoXY(0, 0);
-		//con_outTxt("Cant solve =(\n");
 		printf("FAIL!!!\n");
 		return;
 	}
@@ -139,13 +206,6 @@ int main() {
 		y = read_int("Horse y [1-8]: ");
 	}
 
-
-	//con_init();
-	//con_hideCursor();
-	//con_clearScr();
-	//init_colors();
-
 	horse(x, y);
-	//con_deinit();
 	return 0;
 }
