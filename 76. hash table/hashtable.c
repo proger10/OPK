3cdde4df544db5d62f1a2a0b862444b88f272389
proger10 <vladimirsitnov@gmail.com>
2015-04-30 19:24:38 +0600

    Update hashtable.c

--- 76. hash table/hashtable.c
+++ 76. hash table/hashtable.c
@@ -8,6 +8,9 @@ HashTable *ht_init(size_t size, HashFunction hash, CompareFunction compare){
 	ht->hash = hash;
 	ht->size = size;
 	ht->table = calloc(size, sizeof(SList *));
+	if (ht->table == NULL){
+		return NULL;
+	}
 	return ht;
 }
 
@@ -16,17 +19,25 @@ static void freeentry(void *data, void *null){
 }
 
 void ht_destroy(HashTable *ht){
-	slist_foreach(ht->table, freeentry, NULL);
-	for (int i = 0; i < ht->size; i++){
+	for (size_t i = 0; i < ht->size; i++){
+		slist_foreach(ht->table[i], freeentry, NULL);
 		slist_free(ht->table[i]);
 	}
 	free(ht->table);
+	free(ht);
+}
+
+
+typedef struct _compare{
+	CompareFunction compare;
+	void *data;
+} Compare;
+
+static int compare(void *data, Compare *compare){
+	return compare->compare(data, compare->data);
 }
 
 static Entry *ht_get_low(HashTable *ht, void *key, unsigned hash){
-	if (hash >= ht->size){
-		return NULL;
-	}
 	Compare compare;
 	compare.compare = ht->compare;
 	compare.data = key;
@@ -38,31 +49,18 @@ static Entry *ht_get_low(HashTable *ht, void *key, unsigned hash){
 }
 
 Pointer ht_set(HashTable *ht, void *key, Pointer data){
-	unsigned hash = ht->hash(key);
-	if (hash >= ht->size){
-		return NULL;
+	unsigned hash = ht->hash(key) % ht->size;
+	Entry *entry = ht_get_low(ht, key, hash);
+	if (entry != NULL){
+		Pointer result = entry->data;
+		entry->data = data;
+		return result;
 	}
-	Entry *entry = malloc(sizeof(Entry));
-	if (entry == NULL){
-		return NULL;
-	}
-	entry->data = data;
-	entry->key = key;
 	slist_prepend(ht->table[hash], entry);
+	return NULL;
 }
-
-typedef struct _compare{
-	CompareFunction compare;
-	void *data;
-} Compare;
-
-static int compare(void *data, Compare *compare){
-	return compare->compare(data, compare->data);
-}
-
 Pointer ht_get(HashTable *ht, void *key){
-	unsigned hash = ht->hash(key);
-
+	unsigned hash = ht->hash(key) % ht->size;;
 	Entry *entry = ht_get_low(ht, key, hash);
 	if (entry == NULL)
 		return NULL;
@@ -70,27 +68,44 @@ Pointer ht_get(HashTable *ht, void *key){
 }
 
 int ht_has(HashTable *ht, void *key){
-	unsigned hash = ht->hash(key);
-	if (hash >= ht->size){
-		return NULL;
+	unsigned hash = ht->hash(key) % ht->size;;
+	Entry *entry = ht_get_low(ht, key, hash);
+	if (entry == NULL){
+		return 0;
 	}
-	Compare compare;
-	compare.compare = ht->compare;
-	compare.data = key;
-	SList *list_entry = slist_find_custom(ht->table[hash], key, &compare);
-	if (list_entry == NULL){
-		return NULL;
-	}
+	return 1;
 }
 
-void ht_delete(HashTable *ht, void *key){
+Pointer ht_delete(HashTable *ht, void *key){
+	unsigned hash = ht->hash(key) % ht->size;;
+	Entry *entry = ht_get_low(ht, key, hash);
+	if (entry == NULL){
+		return;
+	}
+	Pointer result = entry->data;
+	slist_remove(ht->table[hash], entry);
+	free(entry);
+	return result;
+}
 
+static void foreach(Entry *entry, void(*f)(void *key, Pointer data)){
+	f(entry->key, entry->data);
 }
 
 void ht_traverse(HashTable *ht, void(*f)(void *key, Pointer data)); {
-
+	for (int i = 0; i < ht->size; i++){
+		slist_foreach(ht->table, foreach,f);
+	}
+	free(ht->table);
 }
 
 void ht_resize(HashTable *ht, size_t new_size){
-
+	SList **table = calloc(new_size, sizeof(SList *));
+	if (table == NULL)
+		return;
+	for (int i = 0; i < ht->size; i++){
+		//slist_foreach(ht->table, foreach, f);
+	}
+	free(ht->table);
+	return ht;
 }
