8e6efcd440a7731453f2b916b4985c8fadba566d
proger10 <vladimirsitnov@gmail.com>
2015-05-13 19:57:41 +0600

    Create d_str.h

---
+++ PASlator/d_str.h
@@ -0,0 +1,17 @@
+#ifndef D_STR_H
+#define D_STR_H
+
+#include <string.h>
+
+typedef struct _d_str{
+	char *str;
+	size_t data_size;
+	size_t size;
+} String;
+
+String *str_create();
+int str_add(String *str, char a);
+char *str_get(String *str);
+void str_destroy(String *str);
+
+#endif
