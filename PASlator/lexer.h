664194c28149e97ef543c3928de231c94e7d3445
proger10 <vladimirsitnov@gmail.com>
2015-05-13 19:58:12 +0600

    Update lexer.h

--- PASlator/lexer.h
+++ PASlator/lexer.h
@@ -10,8 +10,16 @@ typedef enum _lex_type{
 	LEX_LB,
 	LEX_RB,
 	LEX_INT,
-	LEX_REAL
+	LEX_REAL,
+	LEX_UNKNOWN,
+	LEX_NO
 } Lex_type;
+
+typedef enum _state{
+	ST_SEP,
+	ST_ERROR,
+	ST_WORD
+} SM_state;
 
 typedef struct _lex{
 	Lex_type type;
@@ -20,6 +28,7 @@ typedef struct _lex{
 
 typedef struct _lexer{
 	Stream *inp;
+	SM_state state;
 }Lexer;
 
 /*Create lexer from stream*/
@@ -27,6 +36,9 @@ Lexer *lex_create(Stream *in_stream);
 
 /*Get next lex or NULL on failure*/
 Lex *lex_next(Lexer *lexer);
+
+/*Free memmory for lex*/
+void lex_free(Lex *lex);
 
 /*Destroy lexer*/
 void lex_destroy(Lexer *lexer);
