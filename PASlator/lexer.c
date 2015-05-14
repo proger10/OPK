acdf717f57a8b3c05a045e892b1a71cedc0de8c5
proger10 <vladimirsitnov@gmail.com>
2015-05-14 20:00:18 +0600

    Update lexer.c

--- PASlator/lexer.c
+++ PASlator/lexer.c
@@ -32,6 +32,45 @@ static int is_space(int a){
 	return (isspace(a) || (a == EOF));
 }
 
+
+static Lex_type parse_word(String *str){
+	//TODO REFACTOR IT!!!!!!!
+	if (str_compare(str, "begin")){
+		return LEX_BEGIN;
+	}
+	if (str_compare(str, "end")){
+		return LEX_END;
+	}
+	if (str_compare(str, "for")){
+		return LEX_FOR;
+	}
+	if (str_compare(str, "while")){
+		return LEX_WHILE;
+	}
+	if (str_compare(str, "do")){
+		return LEX_DO;
+	}
+	if (str_compare(str, "to")){
+		return LEX_TO;
+	}
+	if (str_compare(str, "repeat")){
+		return LEX_REPEAT;
+	}
+	if (str_compare(str, "until")){
+		return LEX_UNTIL;
+	}
+	if (str_compare(str, "program")){
+		return LEX_PROGRAM;
+	}
+	if (str_compare(str, "shl")){
+		return LEX_SHL;
+	}
+	if (str_compare(str, "shr")){
+		return LEX_SHR;
+	}
+	return LEX_ID;
+}
+
 Lex *lex_next(Lexer *lexer){
 	String *str = str_create();
 	int ret = 0;
@@ -45,7 +84,8 @@ Lex *lex_next(Lexer *lexer){
 		}
 
 		switch (lexer->state){
-			//Starting
+
+		//Starting
 		case ST_SEP:
 			//word case
 			if (is_az(c)){
@@ -62,7 +102,29 @@ Lex *lex_next(Lexer *lexer){
 			//operator separator
 			if (c == ';'){
 				ret = 1;//it's a final stage
-
+				stream_read(lexer->inp);
+				lexer->state = ST_OP_SEP;
+				break;
+			}
+			//end of source
+			if (c == '.'){
+				ret = 1;//it's a final stage
+				stream_read(lexer->inp);
+				lexer->state = ST_DOT;
+				break;
+			}
+			//colon or assign
+			if (c == ':'){
+				stream_read(lexer->inp);
+				lexer->state = ST_COLON;
+				break;
+			}
+			//numbers. integer or real
+			if (is_09(c)){
+				str_add(str, c);
+				stream_read(lexer->inp);
+				lexer->state = ST_INT;
+				break;
 			}
 			//can't recognize lex. something unknown?
 			lexer->state = ST_ERROR;
@@ -70,7 +132,7 @@ Lex *lex_next(Lexer *lexer){
 			ret = 1;
 			break;
 
-			//Word state
+		//Word state
 		case ST_WORD:
 			//read part of word
 			if (is_az(c) || is_09(c)){
@@ -81,6 +143,32 @@ Lex *lex_next(Lexer *lexer){
 			//read something that can't be interpritated as word
 			ret = 1;
 			break;
+
+		case ST_COLON:
+			//decide it is colon or assignment
+			if (c == '='){
+				//it's assignment
+				lexer->state = ST_ASSIGN;
+				stream_read(lexer->inp);
+				ret = 1;
+			}
+			else{
+				//colon
+				ret = 1;
+			}
+			break;
+
+		case ST_INT:
+			//read part of number
+			if (is_09(c)){
+				str_add(str, c);
+				stream_read(lexer->inp);
+				break;
+			}
+			//read something that can't be interpritated as decimal
+			ret = 1;
+			break;
+			
 		default:
 			lexer->state = ST_ERROR;
 			ret = 1;
@@ -92,12 +180,37 @@ Lex *lex_next(Lexer *lexer){
 	Lex *result = NULL;
 	if (lexer->state != ST_SEP){
 		result = malloc(sizeof(Lex));
+		result->type = LEX_NO;
+		result->value = NULL;
 		if (result == NULL)
 			return NULL;
 		else{
 			switch (lexer->state){
 			case ST_WORD:
-				result->type = LEX_WORD;
+				//maybe it is some reserved word
+				result->type = parse_word(str);
+				if (result->type == LEX_ID){
+					result->value = str->str;
+				}
+				else{
+					//bad code. who cares? nobody cares.
+					free(str->str);
+				}
+				break;
+			case ST_OP_SEP:
+				result->type = LEX_OP_SEP;
+				break;
+			case ST_ASSIGN:
+				result->type = LEX_ASSIGN;
+				break;
+			case ST_COLON:
+				result->type = LEX_COLON;
+				break;
+			case ST_DOT:
+				result->type = LEX_DOT;
+				break;
+			case ST_INT:
+				result->type = LEX_INT;
 				result->value = str->str;
 				break;
 			default:
